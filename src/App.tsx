d51e0b3a370dd9308e1d1528f929ea1391990dec
Mateus Abelli <mateusabelli@gmail.com>
2023-06-27 22:19:02 -0300

    Add resources and inferred routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { notificationProvider, RefineSnackbarProvider } from "@refinedev/mui";
+import {
+  notificationProvider,
+  RefineSnackbarProvider,
+  ThemedLayoutV2,
+} from "@refinedev/mui";
 
+import {
+  AutoAwesomeMosaic,
+  AutoAwesomeMotion,
+  Backup,
+  Bookmarks,
+  Dashboard,
+} from "@mui/icons-material/";
 import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
+import { MuiInferencer } from "@refinedev/inferencer/mui";
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <CssBaseline />
@@ -25,14 +36,85 @@ function App() {
             <Refine
               notificationProvider={notificationProvider}
               routerProvider={routerBindings}
-              dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+              dataProvider={{
+                default: dataProvider("https://api.fake-rest.refine.dev"),
+                typicode: dataProvider("https://jsonplaceholder.typicode.com"),
+              }}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
               }}
+              resources={[
+                {
+                  name: "post",
+                  meta: { icon: <Dashboard /> },
+                },
+                {
+                  name: "test",
+                  meta: { icon: <Bookmarks /> },
+                },
+                {
+                  name: "blog_posts",
+                  list: "/blog-posts",
+                  show: "/blog-posts/show/:id",
+                  create: "/blog-posts/create",
+                  edit: "/blog-posts/edit/:id",
+                  meta: {
+                    canDelete: true,
+                    label: "Refine posts",
+                    icon: <AutoAwesomeMotion />,
+                    dataProviderName: "default",
+                    parent: "post",
+                  },
+                },
+                {
+                  name: "posts",
+                  list: "/posts",
+                  show: "/posts/show/:id",
+                  create: "/posts/create",
+                  edit: "/posts/edit/:id",
+                  meta: {
+                    canDelete: false,
+                    label: "Typicode posts",
+                    icon: <AutoAwesomeMosaic />,
+                    dataProviderName: "typicode",
+                    parent: "post",
+                  },
+                },
+                {
+                  name: "fake_resource",
+                  list: "/test",
+                  meta: {
+                    label: "Fake resource",
+                    icon: <Backup />,
+                    parent: "test",
+                  },
+                },
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route
+                  element={
+                    <ThemedLayoutV2>
+                      <Outlet />
+                    </ThemedLayoutV2>
+                  }
+                >
+                  <Route index element={<WelcomePage />} />
+                  <Route path="blog-posts" element={<MuiInferencer />}>
+                    <Route path="show/:id" element={<MuiInferencer />} />
+                    <Route path="edit/:id" element={<MuiInferencer />} />
+                    <Route path="create" element={<MuiInferencer />} />
+                  </Route>
+
+                  <Route path="posts" element={<MuiInferencer />}>
+                    <Route path="show/:id" element={<MuiInferencer />} />
+                    <Route path="edit/:id" element={<MuiInferencer />} />
+                    <Route path="create" element={<MuiInferencer />} />
+                  </Route>
+
+                  <Route path="test" element={<h1>Hello World</h1>} />
+                </Route>
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
